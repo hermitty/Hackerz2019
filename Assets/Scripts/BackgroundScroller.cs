d0f5c816c60dbf4ea7a429c8b7d918195cb7fcf3
hermitty <47337808+hermitty@users.noreply.github.com>
2019-03-29 21:31:07 +0300

    jumping

---
+++ Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour
+{
+    [SerializeField] float startingSpeed = 0f;
+    [SerializeField] float jumpingSpeed = 3f;
+    [SerializeField] float fallingSpeed = 0.03f;
+    Material myMaterial;
+    [SerializeField] Vector2 offset;
+    bool crash = false;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        myMaterial = GetComponent<Renderer>().material;
+        offset = new Vector2 (0f,startingSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Jump();
+    }
+
+    private void Jump()
+    {        
+        if (Input.GetButtonDown("Jump")) offset.y = jumpingSpeed;
+        if(!crash)offset.y -= fallingSpeed; 
+        myMaterial.mainTextureOffset += offset * Time.deltaTime;
+        //TO DO: crash point
+        if (offset.y < -15f)
+        {
+            crash = true;
+            offset.y = 0f;
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float moveSpeedHorizontal = 10f;
+    [SerializeField] float jumpSpeed = 4f;
+    float padding = 0.5f;
+    [SerializeField] GameObject jumpVFX;
+
+    float xMin;
+    float xMax;
+    float yMin;
+    float yMax;
+
+    void Start()
+    {
+        SetUpMoveBoundaries();
+    }
+
+
+    void Update()
+    {
+       Move();
+       Jump();
+    }
+
+    private void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0f, jumpSpeed);
+            Fart();
+        }
+    }
+
+    private void Fart()
+    {
+        GameObject explosion = Instantiate(jumpVFX, transform.position, transform.rotation);
+        explosion.transform.parent = transform;
+        explosion.transform.position = new Vector2(transform.position.x, transform.position.y - 1f);
+
+        Destroy(explosion, 0.7f);
+    }
+
+    private void Move()
+    {
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeedHorizontal;
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
+        var newYPos = Mathf.Clamp(transform.position.y, yMin, yMax);
+        transform.position = new Vector2(newXPos, newYPos);
+        //transform.Translate(Input.acceleration.x, 0, 0);
+        //var newXPos = Mathf.Clamp(transform.position.x, xMin, xMax);
+        //var newYPos = Mathf.Clamp(transform.position.y, yMin, yMax);
+        //transform.position = new Vector2(newXPos, newYPos);
+    }
+
+    private void SetUpMoveBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, -0.4f, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 0.4f, 0)).y - padding;
+    }
+}
